d08e0c0472ad870acb09736b29be05aaad0b8e72
Initial Commit <newhite7070@gmail.com>
2020-06-03 10:41:39 -0400

    String and Arrays done

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -7,6 +7,15 @@ namespace ArrayExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Arrays!");
+            int[] numbers = { 1, 1, 2, 3, 5, 8 };
+            foreach (int i in numbers)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
         }
     }
 }
--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -7,6 +7,12 @@ namespace StringExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Strings!");
+            string ryme = "I would not, could not, in a box. I would not, could not with a fox. \nI will not eat them in a house. I will not eat them with a mouse.";
+            string[] rythem = ryme.Split(' ');
+            Console.WriteLine(string.Join(",", rythem));
+            string[] rythem2 = ryme.Split('.');
+            Console.WriteLine(string.Join(".", rythem2));
+
         }
     }
 }
